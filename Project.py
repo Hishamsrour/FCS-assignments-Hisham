07af4be0f14e6839afb2acd9434492658dae7ce3
Hisham Srour <hishamsrour8@gmail.com>
2024-11-03 23:45:03 +0200

    added adding functions

--- Project.py
+++ Project.py
@@ -14,3 +14,17 @@ class WeDeliver:
         self.drivers = []
         self.cities = []
         self.next_id = 1
+
+    def add_city(self, name):
+        city = City(name)
+        self.cities.append(city)
+        print(f"City {name} added")
+
+    def add_neighbor(self, city_name, neighbor_name):
+        city = next((c for c in self.cities if c.name == city_name), None)
+        neighbor = next((c for c in self.cities if c.name == neighbor_name), None)
+        if city and neighbor:
+            city.neighbors.append(neighbor)
+            print(f"{neighbor_name} added as a neighbor to {city_name}")
+        else:
+            print("One or both cities not found")
